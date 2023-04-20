94e689f4f12ee746b926b459ef1e78c797498def
baileydis <77847645+baileydis@users.noreply.github.com>
2023-04-20 14:15:00 -0400

    Update heatmap.py

--- Data Visualizations/heatmap.py
+++ Data Visualizations/heatmap.py
@@ -12,7 +12,7 @@ fig, ax = plt.subplots(figsize=(50,50))
 
 # plot heatmap
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-plt.savefig('/content/drive/My Drive/DS440/corr_matrix.png')
+
 
 #Correlation with output variable
 cor_target = abs(corr_matrix["Win_Percent"])
