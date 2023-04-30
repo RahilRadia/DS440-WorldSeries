cd2a8f018d563cc76fc9498c857ac51d5d7bc172
RahilRadia <rnr5129@psu.edu>
2023-04-30 10:35:34 -0400

    4/30

--- Data/Feature_Engineering/rawDataCleaning.py
+++ Data/Feature_Engineering/rawDataCleaning.py
@@ -5,6 +5,9 @@ import re
 ##load data##
 playoffs_data = pd.read_csv('Data/Raw/playoffappearances.csv')
 master_ws_data = pd.read_csv('Data/Raw/masterWorldSeries.csv', header=0)
+data_2023 = pd.read_csv('Data/Raw/2023STATS.csv', header = 0)
+
+
 
 ###CLEAN DATA###
 
@@ -16,10 +19,16 @@ master_train["Win_Percent"] = master_ws_data["w"] / (master_ws_data["w"] + maste
 #Add season and team name to array
 test_team = master_test.iloc[:,1:3]
 train_team = master_train.iloc[:,1:3]
+teams_23 = data_2023[['team']]
+
+print(teams_23.head())
 
 #Remove the Wins/Loses columns and case identifier variables Season and Team
 master_test = master_test.drop(columns = ["season","team", "league", 'w', 'l'])
 master_train = master_train.drop(columns = ["season","team", "league", 'w', 'l'])
+data_2023 = data_2023.drop(columns = ["team"])
+
+
 
 #Cast strings to floats and remove JSON char
 master_train = master_train.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
@@ -30,10 +39,18 @@ master_test= master_test.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', 
 master_test = master_test.replace('%', '', regex=True)
 master_test = master_test.astype(float)
 
+data_2023= data_2023.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+data_2023 = data_2023.replace('%', '', regex=True)
+data_2023 = data_2023.astype(float)
+
+
 #Save to processed data folder
 master_train.to_csv('Data/Processed/train.csv')
 master_test.to_csv('Data/Processed/test.csv')
 
 train_team.to_csv('Data/Processed/train_team.csv')
 test_team.to_csv('Data/Processed/test_team.csv')
+
+data_2023.to_csv('Data/Processed/test_23.csv')
+teams_23.to_csv('Data/Processed/team_23.csv')
 
--- Models/Model scripts/Predict.py
+++ Models/Model scripts/Predict.py
@@ -10,9 +10,13 @@ import pickle
 
 train_auto = pd.read_csv('Data/Processed/train_auto.csv', header = 0)
 test_auto = pd.read_csv('Data/Processed/test_auto.csv', header = 0)
+test_23 = pd.read_csv('Data/Processed/test_23.csv', header = 0)
 
 train_team = pd.read_csv('Data/Processed/train_team.csv', header = 0)
 test_team = pd.read_csv('Data/Processed/test_team.csv', header = 0)
+
+
+
 
 #full_train = pd.merge(train_team, train_auto, left_index=True, right_index=True)
 #full_test = pd.merge(test_team, test_auto, left_index=True, right_index=True)
@@ -22,6 +26,7 @@ Y_train_auto = train_auto["Win_Percent"]
 X_test_auto = test_auto
 
 
+
 #Test dataset
 #auto fs
 with open('flaml_model_auto.pkl', 'rb') as f:
@@ -29,13 +34,20 @@ with open('flaml_model_auto.pkl', 'rb') as f:
 
 #predict
 Y_test_auto = flaml_model_auto.predict(X_test_auto)
+Y_test_23 = flaml_model_auto.predict(test_23)
 
+print(Y_test_23)
+print(Y_test_auto)
 
 predictions = test_team
 predictions['Win_Percent'] = Y_test_auto.tolist()
 
+predictions_2023 = test_team
+predictions_2023['Win_Percent'] = Y_test_23.toList()
+
+
 playoff_qualifiers = predictions.groupby('league', group_keys=True).apply(lambda x: x.nlargest(6, ['Win_Percent']))
+playoff_qualifiers_2023 = predictions_2023.groupby('league', group_keys=True).apply(lambda x: x.nlargest(6, ['Win_Percent']))
 
 
-
-playoff_qualifiers.to_csv("Data/Final/2022_predicted_qualifiers.csv")
+playoff_qualifiers_2023.to_csv("Data/Final/2023_predicted_qualifiers.csv")
